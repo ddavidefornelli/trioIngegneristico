287fb2b1feacac06a1ebbdb5159aec21c1900743
ddavidefornelli <fornelli.dv@gmail.com>
2026-03-04 11:37:32 +0100

    hola

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 for i in range(30):
     print("il trio informatico DOMINA")
+
+
+def fibonacci(n):
+    a = 0
+    b = 1
+    for i in range(n):
+        temp = b
+        b += a
+        a = temp
+        print(b)
+
+
+fibonacci(12)
